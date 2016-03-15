dd4eaa2aa3b6dd704f675e6d86034bc0eafa955e
ssuyi <elic.space.cs@gmail.com>
2016-03-15 14:34:44 +0800

    basic scturc of autoprover

--- autoprover.py
+++ autoprover.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python3
+
 import eval, gp, utils
+from proof import Proof
+from utils import parser
+from gp.model import GP
 
 if __name__ == "__main__":
-    pass
+    args = parser.getArgs()
+    proof = Proof(args.file)
+    gpModel = GP(args, proof)
+    GP.start()
+
+    if GP.isProoved():
+        pass
+    else:
+        pass
+
