2ed551214783ee73cbcf7cfd767d9cfdea0139d6
ssuyi <elic.space.cs@gmail.com>
2016-03-16 22:34:09 +0800

    add Chromosome constructor

--- gp/chromosome.py
+++ gp/chromosome.py
@@ -1,2 +1,18 @@
+from random import random, randint
+
 class Chromosome:
-    pass
+    def __init__(self, tactics):
+        self.chromosome = []
+        chromosomeLength = randint(4, 15)
+        print(chromosomeLength)
+        for fragNum in range(chromosomeLength):
+            tactic = tactics[randint(0, len(tactics)-1)]
+            if len(self.chromosome) == 0:
+                self.chromosome.append(tactic)
+            else:
+                while tactic[1] is False and (tactic == self.chromosome[-1]):
+                    tactic = tactics[randint(0, len(tactics)-1)]
+                self.chromosome.append(tactic)
+
+    def showChromosome(self):
+        print(self.chromosome)
