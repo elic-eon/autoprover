3d06e447872880e137548457dc9930f8c956a7a4
ssuyi <elic.space.cs@gmail.com>
2016-03-16 14:14:40 +0800

    add __init__ constructor in class proof

--- proof.py
+++ proof.py
@@ -1,6 +1,9 @@
-class Proof():
+class Proof:
     def __init__(self, inputFile):
         self.theorem = self.readThmFromFile(inputFile)
 
     def readThmFromFile(self, inputFile):
-        pass
+        retList = []
+        for line in inputFile:
+            retList.append(line.strip())
+        return retList
