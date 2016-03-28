4d52ff91ced1f3f8cbec542ed4e5f48f6a329e87
ssuyi <elic.space.cs@gmail.com>
2016-03-28 21:10:15 +0800

    add theoremName attr in proof

--- proof.py
+++ proof.py
@@ -2,6 +2,7 @@ from eval import eval
 class Proof:
     def __init__(self, inputFile):
         self.theorem = self.readThmFromFile(inputFile)
+        self.theoremName = self.getThmNameFromList(self.theorem)
 
     def readThmFromFile(self, inputFile):
         retList = []
@@ -9,8 +10,13 @@ class Proof:
             retList.append(line.strip())
         return retList
 
+    def getThmNameFromList(self, tList):
+        for line in tList:
+            if (line.startswith("Theorem")):
+                return line.split()[1]
+
     def calculateFitness(self, chromosome):
         script = eval.preprocess(self.theorem, chromosome)
         result = eval.runCoqtop(script)
-        return eval.evaluateResult(result)
+        return eval.evaluateResult(result, self.theoremName)
 
